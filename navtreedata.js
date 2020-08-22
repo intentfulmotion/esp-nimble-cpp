db400fc6a159555b2f22505361ea7f0bcb5c913c
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-08-22 17:01:36 +0000

    Deploy h2zero/esp-nimble-cpp to github.com/h2zero/esp-nimble-cpp.git:gh-pages

---
+++ navtreedata.js
@@ -0,0 +1,105 @@
+/*
+ @licstart  The following is the entire license notice for the JavaScript code in this file.
+
+ The MIT License (MIT)
+
+ Copyright (C) 1997-2020 by Dimitri van Heesch
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+ and associated documentation files (the "Software"), to deal in the Software without restriction,
+ including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all copies or
+ substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+ BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+ @licend  The above is the entire license notice for the JavaScript code in this file
+*/
+var NAVTREE =
+[
+  [ "esp-nimble-cpp / NimBLE-Arduino", "index.html", [
+    [ "Overview", "index.html", [
+      [ "Arduino Installation", "index.html#autotoc_md6", null ],
+      [ "ESP-IDF Installation", "index.html#autotoc_md7", [
+        [ "What is NimBLE?", "index.html#autotoc_md5", [
+          [ "v4.0+", "index.html#autotoc_md8", null ],
+          [ "v3.2 & v3.3", "index.html#autotoc_md9", null ]
+        ] ]
+      ] ],
+      [ "Using", "index.html#autotoc_md10", null ],
+      [ "Acknowledgments", "index.html#autotoc_md12", null ]
+    ] ],
+    [ "Improvements and updates", "md_docs__improvements_and_updates.html", [
+      [ "Server", "md_docs__improvements_and_updates.html#autotoc_md1", null ],
+      [ "Client", "md_docs__improvements_and_updates.html#autotoc_md2", null ],
+      [ "General", "md_docs__improvements_and_updates.html#autotoc_md3", null ]
+    ] ],
+    [ "Migrating from Bluedroid to NimBLE", "md_docs__migration_guide.html", [
+      [ "General Information", "md_docs__migration_guide.html#autotoc_md14", [
+        [ "Header Files", "md_docs__migration_guide.html#autotoc_md15", null ],
+        [ "Class Names", "md_docs__migration_guide.html#autotoc_md16", null ],
+        [ "BLE Addresses", "md_docs__migration_guide.html#autotoc_md17", null ]
+      ] ],
+      [ "Server API", "md_docs__migration_guide.html#autotoc_md18", [
+        [ "Services", "md_docs__migration_guide.html#autotoc_md19", null ],
+        [ "Characteristics", "md_docs__migration_guide.html#autotoc_md20", [
+          [ "Arduino specific:", "index.html#autotoc_md11", null ],
+          [ "Originally", "md_docs__migration_guide.html#autotoc_md21", null ],
+          [ "Is Now", "md_docs__migration_guide.html#autotoc_md22", null ],
+          [ "The full list of properties", "md_docs__migration_guide.html#autotoc_md23", null ]
+        ] ],
+        [ "Descriptors", "md_docs__migration_guide.html#autotoc_md24", null ],
+        [ "Server Security", "md_docs__migration_guide.html#autotoc_md27", null ]
+      ] ],
+      [ "Advertising API", "md_docs__migration_guide.html#autotoc_md28", null ],
+      [ "Client API", "md_docs__migration_guide.html#autotoc_md29", [
+        [ "Remote Services", "md_docs__migration_guide.html#autotoc_md30", null ],
+        [ "Remote Characteristics", "md_docs__migration_guide.html#autotoc_md31", null ],
+        [ "Client Security", "md_docs__migration_guide.html#autotoc_md32", null ]
+      ] ],
+      [ "Security API", "md_docs__migration_guide.html#autotoc_md33", null ],
+      [ "Arduino Configuration", "md_docs__migration_guide.html#autotoc_md34", null ]
+    ] ],
+    [ "New User Guide", "md_docs__new_user_guide.html", [
+      [ "Include Files", "md_docs__new_user_guide.html#autotoc_md36", null ],
+      [ "Using the Library", "md_docs__new_user_guide.html#autotoc_md37", null ],
+      [ "Creating a Server", "md_docs__new_user_guide.html#autotoc_md38", null ],
+      [ "Creating a Client", "md_docs__new_user_guide.html#autotoc_md39", null ]
+    ] ],
+    [ "Deprecated List", "deprecated.html", null ],
+    [ "Todo List", "todo.html", null ],
+    [ "Classes", "annotated.html", [
+      [ "Class List", "annotated.html", "annotated_dup" ],
+      [ "Class Index", "classes.html", null ],
+      [ "Class Hierarchy", "hierarchy.html", "hierarchy" ],
+      [ "Class Members", "functions.html", [
+        [ "All", "functions.html", "functions_dup" ],
+        [ "Functions", "functions_func.html", "functions_func" ],
+        [ "Enumerations", "functions_enum.html", null ]
+      ] ]
+    ] ],
+    [ "Files", "files.html", [
+      [ "File List", "files.html", "files_dup" ],
+      [ "File Members", "globals.html", [
+        [ "All", "globals.html", null ],
+        [ "Macros", "globals_defs.html", null ]
+      ] ]
+    ] ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+"_h_i_d_keyboard_types_8h_source.html",
+"class_nim_b_l_e_remote_characteristic.html#ad62c63468995a24d736c4176c9607eeb"
+];
+
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
